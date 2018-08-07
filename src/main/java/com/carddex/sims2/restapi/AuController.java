4d107fd83b5da11a0f3a207097d542fccfd86f3f
uvm <uvm@172.16.3.149>
2018-08-07 16:24:11 +0300

    Поправил докс коммент

--- src/main/java/com/carddex/sims2/restapi/AuController.java
+++ src/main/java/com/carddex/sims2/restapi/AuController.java
@@ -3,11 +3,16 @@ package com.carddex.sims2.restapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carddex.sims2.security.UserSecurityDetailsService;
+import com.carddex.sims2.security.UserAccount;
 import com.carddex.sims2.security.UserLoginDetails;
 import com.carddex.sims2.service.IUserService;
 
@@ -20,35 +25,35 @@ public class AuController {
 	@Autowired
 	IUserService userService;
 
-	/**
-	 * Войти в систему 
-	 * POST /core/api/login 
-	 * Параметры:{ username: [string], // логин
-	 * password: [string] // пароль }
+	/*-
+	 * Войти в систему POST /core/api/login 
+	 * Параметры:
+	 * { 
+	 * 		username: [string], // логин
+	 * 		password: [string] // пароль 
+	 * }
 	 * 
 	 * Возвращаемое значение: 
-	 * В случае успеха: 
+	 * 	В случае успеха: 
 	 * { 
-	 * status: 'success', // статус операции 
-	 * description: 'Login Success', // описание статуса 
-	 * response: { //информация о залогинном пользователе 
-	 *		id: [number], // ид пользователя
-	 *  	username: [string], // имя пользователя 
-	 *  	blocked: [number], // аккаунт заблокирован 
-	 *  	registerDate: [number], // дата регистрации аккаунта 
-	 *  	roles: [
-	 *    		[...number] // ид ролей пользователя 
-	 *  	],
-	 *  	permissions: [ // права пользователя
-	 *    		[...number] // ид разрешений 
-	 *  	]}}
+	 * 		status: 'success', // статус операции 
+	 * 		description: 'Login Success', // описание статуса
+	 * 		response: {//информация о залогинном пользователе
+	 * 			id: [number], // ид пользователя
+	 * 			username: [string], // имя пользователя 
+	 * 			blocked: [number], // аккаунт заблокирован
+	 * 			registerDate: [number], // дата регистрации аккаунта
+	 * 			roles: [
+	 * 				[...number] // ид ролей пользователя ],
+	 * 			permissions: [ // права пользователя [...number] // ид разрешений ]}}
 	 */
 
-	@RequestMapping("/core/api/login")
-	public UserLoginDetails login(@RequestParam(value = "name", defaultValue = "World") String name) {
-		UserLoginDetails userDetails = userDetailsService.restLoadUserByUsername("test@test.com");
-		//UserLoginDetails userDetails = userDetailsService.restLoadUserByUsername("t@test.com");
-		
+	@RequestMapping(value = "/core/api/login", method = RequestMethod.POST)
+	@ResponseBody
+	public UserLoginDetails login(@RequestBody UserAccount account) {
+		String username = account.getUsername();
+		UserLoginDetails userDetails = userDetailsService.restLoadUserByUsername(username);
+
 		return userDetails;
 	}
 }
