858061444d46a585d9daad762ec5ec63d0390993
uvm <uvm@172.16.3.149>
2018-08-07 16:24:57 +0300

    еще

---
+++ src/main/java/com/carddex/sims2/security/UserAccount.java
@@ -0,0 +1,20 @@
+package com.carddex.sims2.security;
+
+public class UserAccount {
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
