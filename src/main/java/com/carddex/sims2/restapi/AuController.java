ad67a2e19941140eb7d79e855c81b5b696704762
uvm <uvm@172.16.3.149>
2018-08-08 08:42:28 +0300

    Добавил код ответа в response

--- src/main/java/com/carddex/sims2/restapi/AuController.java
+++ src/main/java/com/carddex/sims2/restapi/AuController.java
@@ -1,27 +1,23 @@
 package com.carddex.sims2.restapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.carddex.sims2.security.UserSecurityDetailsService;
 import com.carddex.sims2.security.UserAccount;
 import com.carddex.sims2.security.UserLoginDetails;
-import com.carddex.sims2.service.IUserService;
+import com.carddex.sims2.security.UserSecurityDetailsService;
 
 @RestController
 public class AuController {
 
 	@Autowired
 	UserSecurityDetailsService userDetailsService;
-
 
 	/*-
 	 * Войти в систему POST /core/api/login 
@@ -46,10 +42,13 @@ public class AuController {
 
 	@RequestMapping(value = "/core/api/login", method = RequestMethod.POST)
 	@ResponseBody
-	public UserLoginDetails login(@RequestBody UserAccount account) {
+	public ResponseEntity<UserLoginDetails> login(@RequestBody UserAccount account) {
 		String username = account.getUsername();
 		UserLoginDetails userDetails = userDetailsService.restLoadUserByUsername(username);
+		// return userDetails;
+		if (userDetails.getStatus().equals("success"))
+			return ResponseEntity.ok(userDetails);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 
-		return userDetails;
 	}
 }
